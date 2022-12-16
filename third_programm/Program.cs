0d39a23f946d32ae6f546ceb9a481629af48288f
Bogdan <bogdan.fad@yandex.ru>
2022-12-16 23:13:50 +0300

    thirdprogramm complete

---
+++ third_programm/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Задача 4: Выяснить явлеется ли число четным? ");
+Console.Write("введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}

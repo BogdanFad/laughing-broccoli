3c44e20b9ef52d3bb9d77576c324e13882ebcca2
Bogdan <bogdan.fad@yandex.ru>
2022-12-17 01:03:26 +0300

    5 programm complete

---
+++ fifth_programm/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Задача 5: Показать последнюю цифру трёхзначного числа: ");
+int a = new Random().Next (100, 1000);
+Console.WriteLine(a);
+Console.WriteLine("Последней цифрой трёхзначного числа является: ");
+Console.WriteLine(a%10);

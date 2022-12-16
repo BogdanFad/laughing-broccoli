06247069443bac6b6ecda0558858803eda893879
Bogdan <bogdan.fad@yandex.ru>
2022-12-16 21:16:50 +0300

    первая задача выполнена

--- first_programm/Program.cs
+++ first_programm/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World 2!");
+﻿Console.WriteLine("Задача 1: По двум заданным числам проверять является ли первое квадратом второго");
+Console.Write("введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+if(a == b * b)
+{
+    Console.WriteLine("Первое число является квадратом второго числа");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго числа");
+}
